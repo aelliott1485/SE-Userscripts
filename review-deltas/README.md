3060e391bc01dc71871b92899cb036a9388a42d2
Andrew Elliott <aelliott@onatlas.com>
2022-04-22 16:03:44 -0700

    add reeview-deltas script

---
+++ review-deltas/README.md
@@ -0,0 +1,13 @@
+# Review Detlas
+
+This script adds a Time delta column to the history view of a review queue, as well as a button to load rows from the next 19 pages. 
+
+![](loadRowsButton.png)
+![](timeDeltaColumn.png)
+
+## Installation
+
+- Install the userscript with
+[this direct link](https://raw.githubusercontent.com/aellliott1485/SE-Userscripts/master/review-deltas/review-deltas.js).
+
+<sub>developed & tested with Violentmonkey on Firefox</sub>
---
+++ review-deltas/review-deltas.js
@@ -0,0 +1,78 @@
+    // ==UserScript==
+    // @name         Review Deltas
+    // @namespace    http://tampermonkey.net/
+    // @version      0.2
+    // @description  Add Deltas to reviews for users
+    // @author       aelliott1485
+    // @match        https://codereview.stackexchange.com/review/*/history*
+    // @icon         https://www.google.com/s2/favicons?domain=stackexchange.com
+    // @grant        none
+    // ==/UserScript==
+
+    (function($) {
+        'use strict';
+        const MAX_ROWS = 1000;
+        const urlParams = new URLSearchParams(location.search);
+        const filteredByUser = urlParams.has('userId');
+        let page = urlParams.get('page') ?? 1;
+
+        const mainTable = $('div#content table');
+        $("<style type='text/css'> .s-table td.suspect { color:#f00;} </style>").appendTo("head");
+        let nextPageLink = $("a.s-pagination--item:contains('Next')");
+        if (nextPageLink.length) {
+            addLoadDataButton();
+        }
+        const tBody = mainTable.find('tbody');
+        mainTable.find('thead tr').append($('<th>Time delta</th>')[0]);
+
+        const handleRow = (index, row) => {
+            row.insertCell();
+            const timeElement = $(row).find('.history-date');
+            if (timeElement) {
+                row.dataset.ts = timeElement.attr('title');
+            }
+
+            if (!filteredByUser) {
+                const userHref = $(row).find('a[href*="/users/"]').attr('href');
+                if (userHref) {
+                    row.dataset.userId = userHref.split('/')[2];
+                }
+            }
+            if (row.parentNode !== mainTable[0].tBodies[0]) {
+                tBody.append(row);
+            }
+            const olderSibling = row.previousElementSibling;
+            if (olderSibling && (filteredByUser || olderSibling.dataset.userId === row.dataset.userId)) { //and user matches
+                const previousDate = Date.parse(olderSibling.dataset.ts);
+                const rowDate = Date.parse(row.dataset.ts);
+                const delta = (previousDate - rowDate) / 1000;
+                olderSibling.lastElementChild.innerText = `${delta}s`;
+                if (delta < 11) {
+                    olderSibling.lastElementChild.classList.add('suspect');
+                }
+            }
+        }
+        $('div#content table tbody tr').each(handleRow);
+        StackExchange.helpers.showToast('Time Delta column added');
+
+        function addLoadDataButton() {
+            const loadDataButton = $('<a class="ws-nowrap s-btn s-btn__primary ml12">Load Rows From Next 19 Pages</a>');
+            const mainNav = $('div#content nav');
+            mainNav.children(":first").append(loadDataButton);
+            loadDataButton.click(async _ => {
+                loadDataButton.hide();
+                StackExchange.helpers.showToast('loading rows from Next 19 pages');
+                do {
+                    urlParams.set('page', ++page);
+                    const url = location.search ? location.href.replace(location.search, '?' + urlParams.toString()) : location.href + '?' + urlParams.toString();
+                    const data = await $.get(url);
+                    const newDOM = $("<html/>").html(data);
+                    const newRows = newDOM.find('div#content table tbody tr');
+                    newRows.each(handleRow);
+                    nextPageLink = newDOM.find("a.s-pagination--item:contains('Next')");
+
+                } while (tBody[0].rows.length < MAX_ROWS && nextPageLink.length)
+                StackExchange.helpers.showToast('done adding rows from Next 19 pages');
+            });
+        }
+    })(window.jQuery);
